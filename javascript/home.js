685b6a96d3462763dd4e174210c67f9deff12d15
sarveshwar <sarveshwar2005@gmail.com>
2024-05-11 18:38:21 +0530

    style and popup

--- javascript/home.js
+++ javascript/home.js
@@ -1,5 +1,22 @@
 var button = document.querySelector(".add-task");
 button.onclick = ()=>{
-    console.log("hello");
-}
-// button.addEventListener(onclick,()=>{console.log("hello")});
+    button.style.display = "none";
+    var popup = document.createElement("div");
+    popup.className = "popup"
+    var label = document.createElement("label");
+    label.for = "input";
+    label.innerHTML = "enter task";
+    var input = document.createElement("input");
+    input.className = "input";
+    input.type = "text";
+    var btn = document.createElement("btn");
+    btn.className = "addTask";
+    btn.innerHTML = "add";
+    popup.appendChild(label);
+    popup.appendChild(input);
+    popup.appendChild(btn);
+    document.body.appendChild(popup);
+    btn.onclick = ()=>{
+        popup.remove();
+        button.style.display = "block";
+    };};
